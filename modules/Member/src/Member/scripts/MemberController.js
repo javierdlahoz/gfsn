9d7d6455ac844c171504b59dad3ac38f2b97466e
Javier De la Hoz <javierdlahoz@gmail.com>
2017-06-28 07:38:09 -0500

    case when logged in and validated ok

--- modules/Member/src/Member/scripts/MemberController.js
+++ modules/Member/src/Member/scripts/MemberController.js
@@ -2,34 +2,64 @@ angular.module('gfsn').controller('MemberController', MemberController);
 
 function MemberController($scope, MemberService) {
 	var vm = this;
+	vm.MemberService = MemberService;
+	vm.validated = false;
 	vm.localProductId = null;
 	vm.userLoggedIn = false;
+	vm.nonce = null;
+	vm.user = {user_login: null, user_password: null};
+	vm.subscriber = {email: null};
 
 	vm.downloadFiles = function(productId, nonce) {
 		vm.localProductId = productId;
-		console.log('working ok');
-		/*if(vm.userLoggedIn) {
-			getFilesAndDownload(productId);
-		} else {
-			var url = '/wp-json/gfsn-api/membership/logged-in?_wpnonce=' + nonce;
+		vm.nonce = nonce;
 
-			jQuery.get(url, function(data) {
-				if (data.success) {
-					isValidated = data.validated;
-					if(data.validated) {
-						userLoggedIn = true;
-						getFilesAndDownload(productId);
-					}
-					else {
-						jQuery("#notValidated").show();
-						jQuery("#subscribeForm").hide();
-						jQuery("#subscribeModal").modal();
-					}
+		if(vm.userLoggedIn) {
+			vm.getFilesAndDownload(productId);
+		} else {
+			vm.isLoggedIn();
+		}
+	}
+
+	vm.createUser = function() {
+		vm.MemberService.createUser(vm.subscriber, function(response) {
+			console.log(response);
+		});
+	}
+
+	vm.login = function() {
+		vm.MemberService.login(vm.user, function(response) {
+			console.log(response);
+		});
+	}
+
+	vm.isLoggedIn = function() {
+		vm.MemberService.isLoggedIn(vm.nonce, function(response) {
+			if (response.success) { 
+				vm.validated = response.validated;
+				vm.userLoggedIn = true;
+				if(response.validated) {
+					vm.getFilesAndDownload();
 				}
-				else {
-					jQuery("#subscribeModal").modal();	
-				}
-			});
-		}*/
+			} else {
+				vm.userLoggedIn = false;
+			}
+		});
+	}
+
+	vm.getFilesAndDownload = function() {
+		vm.MemberService.getFilesFromProduct(vm.localProductId, function(response) {
+			for(var i = 0; i < response.files.length; i ++ ) {
+				vm.downloadFile(response.files[i]);
+			}
+		});	
+	}
+
+	vm.downloadFile = function(file) {
+		var link = document.createElement('a');
+		link.href = file;
+		link.download = file;
+		document.body.appendChild(link);
+		link.click();
 	}
 }
--- modules/Member/src/Member/scripts/MemberService.js
+++ modules/Member/src/Member/scripts/MemberService.js
@@ -1,9 +1,47 @@
 angular.module('gfsn', []).factory('MemberService', MemberService);
 
 function MemberService($http) {
+	var baseUrl = '/wp-json/gfsn-api/membership';
 	return {
-		test: function() {
-			console.log('this is a test');
+		
+		isLoggedIn: function(nonce, callback) {
+			$http({
+        url: baseUrl + '/logged-in?_wpnonce=' + nonce,
+        method: "GET"
+      }).then(function (response) {
+        return callback(response.data);
+      });
+		},
+
+		createUser: function(user, callback) {
+			$http({
+        url: baseUrl,
+        method: "POST",
+        data: angular.element.param(user),
+        headers: getContentTypes().form
+      }).then(function (response) {
+        return callBack(response.data);
+      });
+		},
+
+		login: function(credentials, callback) {
+			$http({
+        url: baseUrl + '/login',
+        method: "POST",
+        data: angular.element.param(credentials),
+        headers: getContentTypes().form
+      }).then(function (response) {
+        return callBack(response.data);
+      });
+		},
+
+		getFilesFromProduct: function(productId, callback) {
+			$http({
+        url: baseUrl + '/files?product_id=' + productId,
+        method: "GET"
+      }).then(function (response) {
+        return callback(response.data);
+      });
 		}
 	}
 }
