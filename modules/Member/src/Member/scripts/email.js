eb98e241801c70f541e2336595e82fd2f766456b
Javier De la Hoz <javierdlahoz@gmail.com>
2017-06-29 17:27:47 -0500

    email script

---
+++ modules/Member/src/Member/scripts/email.js
@@ -0,0 +1,11 @@
+  var dcq = dcq || [];
+  var dcs = dcs || {}; 
+  _dcs.account = '2017875';
+  
+  (function() {
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript'; dc.async = true; 
+    dc.src = '//tag.getdrip.com/2017875.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dc, s);
+  })();
