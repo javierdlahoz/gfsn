b8064cc71e5e427afc7fad612fc53dc0ece18e85
Javier De la Hoz <javierdlahoz@gmail.com>
2017-07-24 12:36:04 -0500

    about section linking to about us

--- modules/Product/src/Product/views/about_section.php
+++ modules/Product/src/Product/views/about_section.php
@@ -10,21 +10,27 @@
 				<div class="about-section-icon">
 					<i class="fa fa-commenting"></i>
 				</div>
-				<h2><?php echo $who->post_title; ?></h2>
+				<a href="/about-us">
+					<h2><?php echo $who->post_title; ?></h2>
+				</a>
 				<p><?php echo $who->post_content; ?></p>
 			</div>
 			<div class="col-md-4 about-section-content">
 				<div class="about-section-icon">
 					<i class="fa fa-pencil"></i>
 				</div>
-				<h2><?php echo $what->post_title; ?></h2>
+				<a href="/about-us">
+					<h2><?php echo $what->post_title; ?></h2>
+				</a>
 				<p><?php echo $what->post_content; ?></p>
 			</div>
 			<div class="col-md-4 about-section-content">
 				<div class="about-section-icon">
 					<i class="fa fa-question"></i>
 				</div>
-				<h2><?php echo $why->post_title; ?></h2>
+				<a href="/about-us">
+					<h2><?php echo $why->post_title; ?></h2>
+				</a>
 				<p><?php echo $why->post_content; ?></p>
 			</div>
 		</div>
--- modules/Slider/src/Slider/views/slider.php
+++ modules/Slider/src/Slider/views/slider.php
@@ -11,7 +11,7 @@
 						<div class="col-md-6 gfsn-carousel-content">
 							<div class="container">
 								<div class="row">
-									<div class="col-md-12">
+									<div class="col-md-12 text-center">
 										<?php the_content(); ?>
 									</div>
 								</div>
