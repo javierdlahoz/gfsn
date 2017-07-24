0a9753e1ca99a78a4882c44a9d413aa8c6a0992a
Javier De la Hoz <javierdlahoz@gmail.com>
2017-07-24 09:08:11 -0500

    about section

--- modules/Product/src/Product/Initializer/ProductInitializer.php
+++ modules/Product/src/Product/Initializer/ProductInitializer.php
@@ -7,10 +7,20 @@ use Product\Widget\CategoryWidget;
 class ProductInitializer {
 
     function __construct() {
+        $this->enqueStyles();
         add_action('widgets_init', array(&$this, 'registerCategoriesWidget'));
+        add_shortcode('gfsn-about-section', array(&$this, 'aboutSection'));
     }
 
     public function registerCategoriesWidget() {
         register_widget('Product\\Widget\\CategoryWidget');
     }
+
+    public function aboutSection() {
+        include_once __DIR__. '/../views/about_section.php';
+    }
+
+    private function enqueStyles() {
+		wp_enqueue_style('gsfn-about-styles', plugin_dir_url( __FILE__ ) . '../styles/about.css');
+	}
 }
---
+++ modules/Product/src/Product/views/about_section.php
@@ -0,0 +1,27 @@
+<div class="gfsn-about-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 about-section-content">
+				<div class="about-section-icon">
+					<i class="fa fa-commenting"></i>
+				</div>
+				<h2>Test 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt erat purus, in aliquet neque pharetra ac. Nam vel erat quis felis mollis aliquam vitae in mauris. Nulla gravida at mauris ut interdum</p>
+			</div>
+			<div class="col-md-4 about-section-content">
+				<div class="about-section-icon">
+					<i class="fa fa-pencil"></i>
+				</div>
+				<h2>Test 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt erat purus, in aliquet neque pharetra ac. Nam vel erat quis felis mollis aliquam vitae in mauris. Nulla gravida at mauris ut interdum</p>
+			</div>
+			<div class="col-md-4 about-section-content">
+				<div class="about-section-icon">
+					<i class="fa fa-question"></i>
+				</div>
+				<h2>Test 3</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt erat purus, in aliquet neque pharetra ac. Nam vel erat quis felis mollis aliquam vitae in mauris. Nulla gravida at mauris ut interdum</p>
+			</div>
+		</div>
+	</div>
+</div>
