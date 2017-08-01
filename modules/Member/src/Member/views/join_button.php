cbc980e55e3db331f4122e5f459bd6c6b19167f0
Javier De la Hoz <javierdlahoz@gmail.com>
2017-08-01 08:40:42 -0500

    disabling button if logged in

--- modules/Member/src/Member/views/join_button.php
+++ modules/Member/src/Member/views/join_button.php
@@ -1,4 +1,6 @@
+<?php if(wp_get_current_user()->ID === 0) : ?>
 <button type="button" class="btn btn-join gfsn-btn-join" data-toggle="modal" data-target="#subscribeMemberModal">
 	Join Now<br>
 	FREE
 </button>
+<?php endif; 
